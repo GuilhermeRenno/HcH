0495135369ec30595beac8927a0230f4b16758db
Guilherme Renno <guilherme.silveirareno@gmail.com>
2023-09-26 16:46:39 -0300

    paginaLogin
    pagina login finalizada e funcionando corretamente

--- scriptPagLogin.js
+++ scriptPagLogin.js
@@ -1,12 +1,12 @@
 function selecionar(){
     var selecao = document.getElementById("UserSelection").value;
-
-    if(selecao='medico'){
-        window.location.href = "paginaMedico.html";
+    
+    if(selecao=='paciente'){
+        window.location.href = "paginaPaciente.html";
         console.log(window.location.href)
     }
     else{
-        window.location.href = "paginaPaciente.html";
+        window.location.href = "paginaMedico.html";
         console.log(window.location.href)
     }
 }
